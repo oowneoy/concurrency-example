66e7465114ae63b7fa402ae97bc25e041063f304
yeonwoo <olenexxwxxxy@gmail.com>
2024-10-30 00:26:51 +0900

    Lettuce 을 활용해 분산 락 구현

---
+++ src/main/java/com/example/stock/facade/LettuceLockStockFacade.java
@@ -0,0 +1,32 @@
+package com.example.stock.facade;
+
+import com.example.stock.repository.RedisLockRepository;
+import com.example.stock.service.StockService;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class LettuceLockStockFacade {
+
+    private RedisLockRepository redisLockRepository;
+    private StockService stockService;
+
+    public LettuceLockStockFacade(RedisLockRepository redisLockRepository, StockService stockService) {
+        this.redisLockRepository = redisLockRepository;
+        this.stockService = stockService;
+    }
+
+    public void decrease(Long id, Long quantity) throws InterruptedException {
+        while (!redisLockRepository.lock(id)) {
+            // lock 획득 실패 시 100 ms 텀을 두고 lock 획득 재시도
+            Thread.sleep(100);
+        }
+
+        try {
+            stockService.decrease(id, quantity);
+        } finally {
+            redisLockRepository.unlock(id);
+        }
+    }
+}
+
---
+++ src/test/java/com/example/stock/facade/LettuceLockStockFacadeTest.java
@@ -0,0 +1,66 @@
+package com.example.stock.facade;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.RedisConnectionFailureException;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LettuceLockStockFacadeTest {
+    private static final Logger log = LoggerFactory.getLogger(LettuceLockStockFacadeTest.class);
+    @Autowired
+    private LettuceLockStockFacade lettuceLockStockFacade;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        stockRepository.saveAndFlush(new Stock(1L, 100L));
+    }
+
+    @AfterEach
+    public void afterEach() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("동시에 100개의 요청이 들어올 경우 stock 의 수량은 0이다.")
+    public void multipleDecreaseStock() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    lettuceLockStockFacade.decrease(1L, 1L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } catch (RedisConnectionFailureException e) {
+                    log.error("RedisConnectionFailureException", e);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        assertEquals(0, stock.getQuantity());
+    }
+}
