e58bc40b8c74d8fac4899a8470085f171ec62bb7
yeonwoo <olenexxwxxxy@gmail.com>
2024-10-29 18:02:23 +0900

    Pessimistic Lock 방식

---
+++ src/test/java/com/example/stock/service/PessimisticLockStockServiceTest.java
@@ -0,0 +1,66 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class PessimisticLockStockServiceTest {
+    @Autowired
+    private PessimisticLockStockService stockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("quantity 를 1만큼 감소한다.")
+    public void decreaseStock() {
+        stockRepository.saveAndFlush(new Stock(1L, 100L));
+
+        stockService.decrease(1L, 1L);
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertEquals(99, stock.getQuantity());
+    }
+
+
+    @Test
+    @DisplayName("동시에 100개의 요청이 들어올 경우 stock 의 수량은 0이다.")
+    public void multipleDecreaseStock() throws InterruptedException {
+        stockRepository.saveAndFlush(new Stock(1L, 100L));
+
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        assertEquals(0, stock.getQuantity());
+    }
+}
